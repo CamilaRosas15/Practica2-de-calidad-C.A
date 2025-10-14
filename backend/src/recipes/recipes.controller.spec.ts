1907e4565b6da127ac7bcd7d4ac643b4982cc92c
AndresSalva <andres.salvatierra@ucb.edu.bo>
2025-10-14 01:04:18 -0400

    añadiendo las pruebas unitarias para la funcion recomendar

--- backend/src/recipes/recipes.controller.spec.ts
+++ backend/src/recipes/recipes.controller.spec.ts
@@ -6,22 +6,38 @@ describe('RecipesController', () => {
   let controller: RecipesController;
 
   const recipesServiceMock = {
-    getRecipeById: jest.fn(),
-    recomendarRecetas: jest.fn(),
+    getById: jest.fn(),
+    recomendarReceta: jest.fn(),
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RecipesController],
       providers: [
-        { provide: RecipesService, useValue: recipesServiceMock }, // << mock
+        { provide: RecipesService, useValue: recipesServiceMock },
       ],
     }).compile();
 
     controller = module.get<RecipesController>(RecipesController);
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('recomendar', () => {
+    it('debe llamar al servicio con el body y devolver su resultado', async () => {
+      const requestBody = { gustos: ['pollo'], tiempo_max: 20 };
+      const serviceResponse = { opciones: [{ id_receta: 2, titulo: 'Pollo al Horno' }] };
+
+      recipesServiceMock.recomendarReceta.mockResolvedValue(serviceResponse);
+
+      const result = await controller.recomendar(requestBody);
+
+      expect(recipesServiceMock.recomendarReceta).toHaveBeenCalledWith(requestBody);
+
+      expect(result).toEqual(serviceResponse);
+    });
   });
 });

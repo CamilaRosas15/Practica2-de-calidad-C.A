d899885f8450a760a48da538075bfd0e806263e9
AndresSalva <andres.salvatierra@ucb.edu.bo>
2025-10-14 01:17:32 -0400

    añadiendo pruebas unitarias de getById

--- backend/src/recipes/recipes.controller.spec.ts
+++ backend/src/recipes/recipes.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecipesController } from './recipes.controller';
 import { RecipesService } from './recipes.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('RecipesController', () => {
   let controller: RecipesController;
@@ -19,14 +20,41 @@ describe('RecipesController', () => {
     }).compile();
 
     controller = module.get<RecipesController>(RecipesController);
-    jest.clearAllMocks();
+    jest.clearAllMocks(); 
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
+  describe('getById', () => {
+    // TC 1: Camino Exitoso
+    it('debe devolver una receta si se encuentra', async () => {
+      const recipeMock = { id_receta: 1, nombre: 'Torta de Chocolate' };
+      recipesServiceMock.getById.mockResolvedValue(recipeMock);
+
+      const result = await controller.getById('1');
+
+      expect(recipesServiceMock.getById).toHaveBeenCalledWith(1);
+      expect(result).toEqual(recipeMock);
+    });
+
+    // TC 2: Receta No Encontrada
+    it('debe lanzar NotFoundException si el servicio devuelve null', async () => {
+      recipesServiceMock.getById.mockResolvedValue(null);
+      await expect(controller.getById('404')).rejects.toThrow(NotFoundException);
+      expect(recipesServiceMock.getById).toHaveBeenCalledWith(404);
+    });
+
+    // TC 3: ID Inválido
+    it('debe lanzar NotFoundException si el ID no es numérico', async () => {
+      await expect(controller.getById('abc')).rejects.toThrow(NotFoundException);
+      expect(recipesServiceMock.getById).not.toHaveBeenCalled();
+    });
+  });
+
   describe('recomendar', () => {
+    // TC 1: Camino Exitoso
     it('debe llamar al servicio con el body y devolver su resultado', async () => {
       const requestBody = { gustos: ['pollo'], tiempo_max: 20 };
       const serviceResponse = { opciones: [{ id_receta: 2, titulo: 'Pollo al Horno' }] };
@@ -36,7 +64,6 @@ describe('RecipesController', () => {
       const result = await controller.recomendar(requestBody);
 
       expect(recipesServiceMock.recomendarReceta).toHaveBeenCalledWith(requestBody);
-
       expect(result).toEqual(serviceResponse);
     });
   });
